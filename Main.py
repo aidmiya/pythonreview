21ad36d0da86b2343b39f1c36914ac344feffd9d
0508akkun <e17-ffkg@kure.kosen-ac.jp>
2019-11-28 16:23:40 +0900

    :tada:FizzBuzz1を追加

--- Main.py
+++ Main.py
@@ -1,2 +1,13 @@
+def FizzBuzz1():
+    foriinrange(0, 30)
+	if(i+1) % 15 == 0:
+	   print('FizzBuzz')
+	elif(i+1)%5==0:
+	   print('Buzz')
+	elif(i+1)%3==0:
+	   print('Fizz')
+	else:
+	   print(i)
+
 if __name__ == "__main__":
     print('Hello World!')
