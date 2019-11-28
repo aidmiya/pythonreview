3f942941c35d3244a8a4715ba7420ada2577bfc2
aidmiya <31529568+aidmiya@users.noreply.github.com>
2019-11-28 17:06:44 +0900

    [delete] sho2004のコード

--- Main.py
+++ Main.py
@@ -1,14 +1,3 @@
-def FizzBuzz2(b, e):
-    for i in range(b, e+1):
-        if i % 15 == 0:
-            print('FizzBuzz')
-        elif i % 5 == 0:
-            print('Buzz')
-        elif i % 3 == 0:
-            print('Fizz')
-        else:
-            print(i)
-
 if __name__ == "__main__":
     print('Hello World!')
     print('FizzBuzz を行います．')
